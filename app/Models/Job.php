80e89217ba43c21de907c3d58b16fff13bfecc8b
oleksandr <oleksandr@epunks.de>
2025-08-26 14:21:02 +0200

    added employer model and factory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Job extends Model
 {
-    protected $table = 'job_listings';
+    use HasFactory;
 
+    protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
 }
